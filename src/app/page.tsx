d4e22c70a9bf5c83d9f44fd89ee3922b263aedcc
Fabian <fabian.hauser@freenet.de>
2026-04-20 11:57:10 +0200

    feat: wire all sections into page.tsx, complete dark terminal portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,21 @@
-'use client';
-
-import {
-  Box,
-  Button,
-  Checkbox,
-  ClientOnly,
-  HStack,
-  Heading,
-  Progress,
-  RadioGroup,
-  Skeleton,
-  VStack,
-} from '@chakra-ui/react';
-import Image from 'next/image';
-import { ColorModeToggle } from '../components/ui/color-mode-toggle';
+import Navigation from '@/components/layout/Navigation';
+import HeroSection from '@/components/sections/HeroSection';
+import AboutSection from '@/components/sections/AboutSection';
+import ExperienceSection from '@/components/sections/ExperienceSection';
+import SkillsSection from '@/components/sections/SkillsSection';
+import ContactSection from '@/components/sections/ContactSection';
 
 export default function Page() {
   return (
-    <Box textAlign="center" fontSize="xl" pt="30vh">
-      <VStack gap="8">
-        <Image
-          alt="chakra logo"
-          src="/static/logo.svg"
-          width="80"
-          height="80"
-        />
-        <Heading size="2xl" letterSpacing="tight">
-          Welcome to Chakra UI v3 + Next.js (App)
-        </Heading>
-
-        <HStack gap="10">
-          <Checkbox.Root defaultChecked>
-            <Checkbox.HiddenInput />
-            <Checkbox.Control>
-              <Checkbox.Indicator />
-            </Checkbox.Control>
-            <Checkbox.Label>Checkbox</Checkbox.Label>
-          </Checkbox.Root>
-
-          <RadioGroup.Root display="inline-flex" defaultValue="1">
-            <RadioGroup.Item value="1" mr="2">
-              <RadioGroup.ItemHiddenInput />
-              <RadioGroup.ItemControl>
-                <RadioGroup.ItemIndicator />
-              </RadioGroup.ItemControl>
-              <RadioGroup.ItemText lineHeight="1">
-                Radio
-              </RadioGroup.ItemText>
-            </RadioGroup.Item>
-
-            <RadioGroup.Item value="2">
-              <RadioGroup.ItemHiddenInput />
-              <RadioGroup.ItemControl>
-                <RadioGroup.ItemIndicator />
-              </RadioGroup.ItemControl>
-              <RadioGroup.ItemText lineHeight="1">
-                Radio
-              </RadioGroup.ItemText>
-            </RadioGroup.Item>
-          </RadioGroup.Root>
-        </HStack>
-
-        <Progress.Root width="300px" value={65} striped>
-          <Progress.Track>
-            <Progress.Range />
-          </Progress.Track>
-        </Progress.Root>
-
-        <HStack>
-          <Button>Let's go!</Button>
-          <Button variant="outline">
-            bun install @chakra-ui/react
-          </Button>
-        </HStack>
-      </VStack>
-
-      <Box pos="absolute" top="4" right="4">
-        <ClientOnly
-          fallback={<Skeleton w="10" h="10" rounded="md" />}
-        >
-          <ColorModeToggle />
-        </ClientOnly>
-      </Box>
-    </Box>
+    <>
+      <Navigation />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <ExperienceSection />
+        <SkillsSection />
+        <ContactSection />
+      </main>
+    </>
   );
 }
